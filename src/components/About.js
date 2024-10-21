7396e0668b2bbee73f2a63ba2add9b4250e9dbb5
SirLoddington <benfischerj@gmail.com>
2024-10-21 13:51:46 +1100

    about with portrait

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,31 @@
+import portrait from "./portrait.jpg";
 export default function About() {
   return (
     <section
       id="about"
       className="min-h-screen bg-gray-200 flex flex-col justify-center items-center"
     >
-      <h2 className="text-3xl font-bold">About Me</h2>
-      <div>
-        <p>Hi I'm Ben</p>
-        <p>
-          I've been fascinated with AI since childhood. It's fascinating from so
-          many angles: mathematical, philosophical, practical, ethical. Now, I'm
-          an AI engineer with full stack skills from ML to frontend.
-        </p>{" "}
+      <div className="w-4/5">
+        <div className="flex items-center space-x-10">
+          <div className="w-1/2">
+            <p className="font-bold text-lg">Hi I'm Ben</p>
+            <p>
+              I enjoy using AI in new and creative ways and applying it to
+              interesting problems.
+            </p>
+            <p>
+              AI is fascinating from so many angles: mathematical,
+              philosophical, practical, ethical.
+            </p>
+            <p>There are so many cool ways it can be understood and used!</p>
+
+            <p>
+              In my spare time I enjoy cooking, reading, writing, travelling and
+              basketball.
+            </p>
+          </div>
+          <img src={portrait} className="max-h-screen" />
+        </div>
       </div>
     </section>
   );
